4e8f79f6ba82f97801e334e7cc629f1b4fe5dfce
GVCC-2022 <gyanvarshacoachingcenter@gmail.com>
2026-07-05 18:25:03 +0530

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,24 +1,23 @@
-importScripts('https://www.gstatic.com/firebasejs/12.2.1/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/12.2.1/firebase-messaging-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.6.1/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.6.1/firebase-messaging-compat.js');
 
 firebase.initializeApp({
     apiKey: "AIzaSyBJX_dyLK80p2rNqSVvGY67TpWV7e_UdPI",
-    authDomain: "gvcc-database.firebaseapp.com",
     projectId: "gvcc-database",
-    storageBucket: "gvcc-database.firebasestorage.app",
     messagingSenderId: "1063216875940",
     appId: "1:1063216875940:web:a4ee2f39b01896b44c5931"
 });
 
 const messaging = firebase.messaging();
 
+// जब ऐप बंद होगा, तब यह कोड फोन की स्क्रीन पर नोटिफिकेशन गिराएगा
 messaging.onBackgroundMessage(function(payload) {
-  console.log('Background message received: ', payload);
-  const notificationTitle = payload.notification.title;
-  const notificationOptions = {
-    body: payload.notification.body,
-    // 👇 यहाँ हमने पूरा लिंक डाल दिया है
-    icon: 'https://gvcc.online/20260620_144347.png'
-  };
-  self.registration.showNotification(notificationTitle, notificationOptions);
+    console.log("Background Message Received:", payload);
+    const notificationTitle = payload.notification.title || "GVCC New Update";
+    const notificationOptions = {
+        body: payload.notification.body,
+        icon: '/20260620_144347.png', // आपके कोचिंग का लोगो
+        badge: '/20260620_144347.png'
+    };
+    return self.registration.showNotification(notificationTitle, notificationOptions);
 });
